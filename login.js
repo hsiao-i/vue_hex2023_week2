aaaf13649340b37e83e36b1b3d48d8b2ebc49930
駱筱尹 <eva090029@eri.com.tw>
2023-01-11 17:25:27 +0800

    init commit

---
+++ login.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.user.username, this.user.password);
+      const baseUrl = 'https://vue3-course-api.hexschool.io/v2'
+      
+      const url = `${baseUrl}/admin/signin`
+      
+
+      axios.post(url, this.user)
+      .then((res) => {
+        console.log(res);
+        const { expired, token } = res.data
+        document.cookie = `hexToken=${ token };expired=${ new Date(expired) }`
+        window.location.href='product.html'
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+})
+app.mount('#app')
